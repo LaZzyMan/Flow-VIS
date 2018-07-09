69918601086613f4f7af46ddc529362a1315f5d2
zz <924797163@qq.com>
2018-07-09 16:18:43 +0800

    添加站点和多边形图层

--- src/components/MapView/index.js
+++ src/components/MapView/index.js
@@ -6,6 +6,8 @@ import TWEEN from 'tween.js'
 import { hex2Rgba } from '../../utils'
 
 const roadData = require('../../data/road_selected.geojson')
+const station = require('../../data/site_point_selected.geojson')
+const voronoi = require('../../data/site_polygon_selected.geojson')
 
 const roadColorMap = [
   hex2Rgba('#CB3466', 255),
@@ -61,6 +63,18 @@ class MapView extends Component {
     const { viewport } = this.state
     const layers = [
       new GeoJsonLayer({
+        id: 'voronoi-layer',
+        data: voronoi,
+        pickable: true,
+        stroked: true,
+        filled: true,
+        lineJointRounded: true,
+        fp64: true,
+        getFillColor: hex2Rgba('#3b8d99', 100),
+        getLineColor: hex2Rgba('#3b8d99', 255),
+        getLineWidth: 1,
+      }),
+      new GeoJsonLayer({
         id: 'road-layer',
         data: roadData,
         pickable: true,
@@ -71,6 +85,14 @@ class MapView extends Component {
         getLineWidth: f => Number(f.properties.Width),
         lineWidthMinPixels: 0.5,
         lineWidthMaxPixels: 2,
+      }),
+      new GeoJsonLayer({
+        id: 'station-layer',
+        data: station,
+        pickable: true,
+        fp64: true,
+        getFillColor: hex2Rgba('#aa4b6b', 200),
+        getRadius: 5,
       }),
     ]
     return (
