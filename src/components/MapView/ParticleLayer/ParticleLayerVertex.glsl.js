bd730ed03549fe07d133256ace155c0d3c41bc99
zz <924797163@qq.com>
2018-07-16 09:46:08 +0800

    glsl编写着色器

--- src/components/MapView/ParticleLayer/ParticleLayerVertex.glsl.js
+++ src/components/MapView/ParticleLayer/ParticleLayerVertex.glsl.js
@@ -1,62 +1,61 @@
+// uniform: bbox, boundx, boundy, data, pixelRatio
+// attribute: position, posFrom
+// varying: vColor
+// gl_Positon gl_PointSize
+
 export default `\
 #define SHADER_NAME particle-layer-vertex-shader
 
 #define HEIGHT_FACTOR 25.
 #define ELEVATION_SCALE 100.
 
-uniform sampler2D dataFrom;
-uniform sampler2D dataTo;
-uniform sampler2D elevationTexture;
-uniform float delta;
+uniform sampler2D data;
 uniform float pixelRatio;
 
 uniform vec4 bbox;
-uniform vec2 size;
-uniform vec2 bounds0;
-uniform vec2 bounds1;
-uniform vec2 bounds2;
-uniform vec4 elevationBounds;
-uniform vec2 elevationRange;
-uniform float zScale;
+uniform vec2 boundx;
+uniform vec2 boundy;
 
 attribute vec3 positions;
 attribute vec4 posFrom;
 // attribute vec3 vertices;
 
 varying vec4 vColor;
-varying float vAltitude;
+// varying float vAltitude;
 
-float getAltitude(vec2 lngLat) {
-  vec2 texCoords = (lngLat - elevationBounds.xy) / (elevationBounds.zw - elevationBounds.xy);
-  vec4 elevation = texture2D(elevationTexture, texCoords);
+// float getAltitude(vec2 lngLat) {
+//   vec2 texCoords = (lngLat - elevationBounds.xy) / (elevationBounds.zw - elevationBounds.xy);
+//   vec4 elevation = texture2D(elevationTexture, texCoords);
 
-  return mix(elevationRange.x, elevationRange.y, elevation.r);
-}
+//   return mix(elevationRange.x, elevationRange.y, elevation.r);
+// }
 
 void main(void) {
   // position in texture coords
   float x = (posFrom.x - bbox.x) / (bbox.y - bbox.x);
   float y = (posFrom.y - bbox.z) / (bbox.w - bbox.z);
   vec2 coord = vec2(x, 1. - y);
-  vec4 texel = mix(texture2D(dataFrom, coord), texture2D(dataTo, coord), delta);
+  vec4 texel = texture2D(data, coord);
 
-  vAltitude = getAltitude(posFrom.xy);
-  //float wind = (texel.y - bounds1.x) / (bounds1.y - bounds1.x);
-  float wind = 0.05 + (texel.y - bounds1.x) / (bounds1.y - bounds1.x) * 0.9;
+  // vAltitude = getAltitude(posFrom.xy);
+  // float wind = (texel.y - bounds1.x) / (bounds1.y - bounds1.x);
+  float vx = 0.05 + (texel.x - boundx.x) / (boundx.y - boundx.x) * 0.9;
+  float vy = 0.05 + (texel.y - boundy.x) / (boundy.y - boundx.x) * 0.9;
+  float v = length(vec2(vx, vy));
 
   vec2 pos = project_position(posFrom.xy);
-  float elevation = project_scale((texel.w) * ELEVATION_SCALE);
+  // float elevation = project_scale((texel.w) * ELEVATION_SCALE);
 
-  vec3 extrudedPosition = vec3(pos.xy, elevation + 1.0) + positions;
+  vec3 extrudedPosition = vec3(pos.xy, 1.0) + positions;
   vec4 position_worldspace = vec4(extrudedPosition, 1.0);
   gl_Position = project_to_clipspace(position_worldspace);
   gl_PointSize = 3.5 * pixelRatio / 2.0;
 
   // OLD
-  // float alpha = mix(0., 0.8, pow(wind, .5));
+  // float alpha = mix(0., 0.8, pow(v, .5));
 
   // NEW
-  float alpha = mix(0., .8, pow(wind, .8));
+  float alpha = mix(0., .8, pow(v, .8));
 
   if (texel.x == 0. && texel.y == 0. && texel.z == 0.) {
     alpha = 0.;
--- src/components/MapView/ParticleLayer/TransformFeedbackVertex.glsl.js
+++ src/components/MapView/ParticleLayer/TransformFeedbackVertex.glsl.js
@@ -1,3 +1,7 @@
+// uniform: bbox, boundx, boundy, flip, data, time
+// attribute: posFrom
+// gl_Positon
+
 export default `\
 #define SHADER_NAME particle-feedback-vertex-shader
 
@@ -9,18 +13,14 @@ export default `\
 #define DELTA 5.
 #define FACTOR .05
 
-uniform sampler2D dataFrom;
-uniform sampler2D dataTo;
-uniform float delta;
+uniform sampler2D data;
 uniform float time;
 
 uniform float flip;
 uniform vec4 bbox;
-uniform vec2 bounds0;
-uniform vec2 bounds1;
-uniform vec2 bounds2;
+uniform vec2 boundx;
+uniform vec2 boundy;
 
-//attribute vec3 positions;
 attribute vec4 posFrom;
 
 float rand(vec2 co){
@@ -32,50 +32,58 @@ void main(void) {
   float x = (posFrom.x - bbox.x) / (bbox.y - bbox.x);
   float y = (posFrom.y - bbox.z) / (bbox.w - bbox.z);
   vec2 coord = vec2(x, 1. - y);
-  vec4 texel1 = texture2D(dataFrom, coord);
-  vec4 texel2 = texture2D(dataTo, coord);
-  vec4 texel = mix(texel1, texel2, delta);
+  vec4 texel = texture2D(data, coord);
 
-  // angle
-  float angle = texel.x * PI4;
-  float anglePast = posFrom.z;
-  if (angle < 0.) {
-    angle += PI * 2.;
-  }
-  if (anglePast > -1.) {
-    if (angle > anglePast && abs(angle - anglePast) > abs(angle - (anglePast + PI * 2.))) {
-      anglePast += PI * 2.;
-    } else if (angle < anglePast && abs(anglePast - angle) > abs(anglePast - (angle + PI * 2.))) {
-      angle += PI * 2.;
-    }
-    angle = angle * FACTOR + anglePast * (1. - FACTOR);
-  }
+  // calculate speed in direction of x and y
+  float currentx = posFrom.z;
+  float currenty = posFrom.w;
+  float fx = 0.05 + 0.9 * (texel.x - boundx.x) / (boundx.y - boundx.x);
+  float fy = 0.05 + 0.9 * (texel.y - boundy.x) / (boundy.y - boundy.x);
+  currentx = currentx + fx;
+  currenty = currenty + fy;
+
+  // calculate next postion
+  vec2 offset = vec2(currentx, currenty) * 0.2;
+  vec2 offsetPos = posFrom.xy + offset;
+  vec4 endPos = vec4(offsetPos, currentx, currenty);
+
+  // calculate angle
+  // float angle = texel.x * PI4;
+  // float anglePast = posFrom.z;
+  // if (angle < 0.) {
+  //   angle += PI * 2.;
+  // }
+  // if (anglePast > -1.) {
+  //   if (angle > anglePast && abs(angle - anglePast) > abs(angle - (anglePast + PI * 2.))) {
+  //     anglePast += PI * 2.;
+  //   } else if (angle < anglePast && abs(anglePast - angle) > abs(anglePast - (angle + PI * 2.))) {
+  //     angle += PI * 2.;
+  //   }
+  //   angle = angle * FACTOR + anglePast * (1. - FACTOR);
+  // }
 
   // wind speed
-  float wind = 0.05 + 0.9 * (texel.y - bounds1.x) / (bounds1.y - bounds1.x);
-  float windPast = posFrom.w;
-  if (windPast > -1.) {
-    wind = wind * FACTOR + windPast * (1. - FACTOR);
-  }
+  // float wind = 0.05 + 0.9 * (texel.y - bounds1.x) / (bounds1.y - bounds1.x);
+  // float windPast = posFrom.w;
+  // if (windPast > -1.) {
+  //   wind = wind * FACTOR + windPast * (1. - FACTOR);
+  // }
 
-  vec2 offset = vec2(cos(angle), sin(angle)) * wind * 0.2;
-  vec2 offsetPos = posFrom.xy + offset;
-
-  vec4 endPos = vec4(offsetPos, mod(angle, PI * 2.), wind);
-
-  // if out of bounds then map to random position
+  // if 1. out of bounds 2. movement is too little 
+  // then map to random position
   float r1 = rand(vec2(posFrom.x, offset.x + time));
   float r2 = rand(vec2(posFrom.y, offset.y + time));
   r1 = r1 * (bbox.y - bbox.x) + bbox.x;
   r2 = r2 * (bbox.w - bbox.z) + bbox.z;
   vec2 randValues = vec2(r1, r2);
-
-  // endPos = vec4(offsetPos, randValues);
+  // if(offsetPos.x < bbox.x || offsetPos.x > bbox.y || offsetPos.y < bbox.z || offsetPos.y > bbox.w) {
+  //   endPos.xy = randValues;
+  // }
   endPos.xy = mix(offsetPos, randValues,
     float(offsetPos.x < bbox.x || offsetPos.x > bbox.y ||
       offsetPos.y < bbox.z || offsetPos.y > bbox.w));
   endPos.xy = mix(endPos.xy, randValues, float(length(offset) < EPSILON));
-  endPos.xy = mix(endPos.xy, randValues, float(texel.x == 0. && texel.y == 0. && texel.z == 0.));
+  // endPos.xy = mix(endPos.xy, randValues, float(texel.x == 0. && texel.y == 0.));
   if (flip > 0.) {
     if (abs(abs(fract(endPos.x)) - flip / 10.) < EPSILON) {
       endPos.xy = randValues;
